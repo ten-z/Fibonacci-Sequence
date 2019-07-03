37088f192445f270338dbb99ddfd2abf107912fa
ten-z <34624233+ten-z@users.noreply.github.com>
2019-07-03 15:13:24 +0800

    Update 2 Recursion With  Memoization.java

--- 2 Recursion With  Memoization.java
+++ 2 Recursion With  Memoization.java
@@ -1,69 +1,28 @@
-{\rtf1\ansi\ansicpg936\cocoartf1671\cocoasubrtf500
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset134 PingFangSC-Regular;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww12600\viewh10200\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-
-\f0\fs28 \cf0 //
-\f1 \'ec\'b3\'b2\'a8\'c4\'c7\'c6\'f5\'ca\'fd\'c1\'d0
-\f0 --
-\f1 \'bc\'c7\'d2\'e4\'bb\'af\'cb\'d1\'cb\'f7
-\f0 \
-public class Solution \{\
-    public static void main(String []args) \{\
-		System.out.print(fibonacci(10));\
-    \}\
-	\
-    public static int fibonacci(int n) \{\
-		if(n<2)\
-			return n;\
-		//
-\f1 \'b3\'f5\'ca\'bc\'bb\'af
-\f0 memo
-\f1 \'ca\'fd\'d7\'e9
-\f0 ,
-\f1 \'b4\'d3
-\f0 1
-\f1 \'a1\'ab
-\f0 n
-\f1 \'a3\'ac\'cb\'f9\'d2\'d4\'b3\'a4\'b6\'c8\'ce\'aa
-\f0 n+1\
-		int[] memo = new int[n+1];\
-		return fibonacciMemorize(n, memo);\
-	\}\
-	\
-	//
-\f1 \'d3\'c3
-\f0 memo
-\f1 \'ca\'fd\'d7\'e9\'b4\'e6\'b4\'a2\'bc\'c6\'cb\'e3\'bd\'e1\'b9\'fb
-\f0 \
-	public static int fibonacciMemorize(int n, int[] memo) \{\
-		if(n==1 || n==2)\{\
-			return 1;\
-		\}\
- \
-		//
-\f1 \'c8\'e7\'b9\'fb
-\f0 memo(n)
-\f1 \'d3\'d0\'d6\'b5\'a3\'ac\'d6\'b1\'bd\'d3\'b7\'b5\'bb\'d8
-\f0 memo[n]
-\f1 \'a3\'ac\'bc\'f5\'c9\'d9\'b5\'dd\'b9\'e9\'b4\'ce\'ca\'fd\'a3\'ac\'d3\'c9\'d3\'da
-\f0 n
-\f1 \'b4\'d3
-\f0 1
-\f1 \'bf\'aa\'ca\'bc\'a3\'ac
-\f0 memo(0)=0
-\f1 \'b2\'bb\'ca\'dc\'d3\'b0\'cf\'ec\'a1\'a3
-\f0 \
-		if(memo[n]==0)\{\
-			//
-\f1 \'c8\'e7\'b9\'fb\'b5\'b1\'c7\'b0
-\f0 f(n)
-\f1 \'c3\'bb\'b1\'bb\'bc\'c6\'cb\'e3\'b9\'fd\'a3\'ac\'d4\'f2\'bc\'c6\'cb\'e3\'ba\'f3\'bd\'ab\'bd\'e1\'b9\'fb\'b4\'e6\'c8\'eb
-\f0 memo[n]\
-			memo[n] = fibonacciMemorize(n-1, memo) + fibonacciMemorize(n-2, memo);\
-		\}\
-		return memo[n];\
-	\}\
-\}}
+//斐波那契数列--记忆化搜索
+public class Solution {
+	public static void main(String []args) {
+		System.out.print(fibonacci(10));
+	}
+	
+	public static int fibonacci(int n) {
+		if(n<2)
+			return n;
+		//初始化memo数组,从1～n，所以长度为n+1
+		int[] memo = new int[n+1];
+		return fibonacciMemorize(n, memo);
+	}
+	
+	//用memo数组存储计算结果
+	public static int fibonacciMemorize(int n, int[] memo) {
+		if(n==1 || n==2){
+			return 1;
+		}
+ 
+		//如果memo(n)有值，直接返回memo[n]，减少递归次数，由于n从1开始，memo[0]=0不受影响。
+		if(memo[n]==0){
+			//如果当前f(n)没被计算过，则计算后将结果存入memo[n]
+			memo[n] = fibonacciMemorize(n-1, memo) + fibonacciMemorize(n-2, memo);
+		}
+		return memo[n];
+	}
+}
